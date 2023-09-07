91a7df9fae55d84f9ae7ebb609319100c89287f2
Christopher Lozzi <clozzi12@gmail.com>
2023-09-07 13:38:56 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -18,10 +18,8 @@ fetch ("https://dog.ceo/api/breeds/list/all")
 let dogList = document.getElementById('dog-breeds')
 
 function displayBreeds(data) {
-  console.log(data)
   for (el in data.message) {
     let dogBreed = document.createElement('li')
-
     dogBreed.textContent = el
     dogList.append(dogBreed)
     dogBreed.addEventListener('click', () => {
@@ -31,26 +29,24 @@ function displayBreeds(data) {
 }
 
 const dropdown = document.getElementById('breed-dropdown')
-dropdown.addEventListener('click', filterDogs);
+// dropdown.addEventListener('click', dropdownAction);
 
-function filterDogs() {
+// function dropdownAction() {
+//   const dropdownPick = document.getElementById('breed-dropdown').value;
+//   console.log(dropdownPick);
+//   dogList
+// }
+
+dropdown.addEventListener('change', () => {
   const dropdownPick = document.getElementById('breed-dropdown').value;
-  console.log(dropdownPick);
-  dogList.innerHTML = "";
-  fetch ("https://dog.ceo/api/breeds/list/all")
-  .then(res => res.json())
-  .then (data => displayFilteredBreed(data));
-}
-
-function displayFilteredBreed(data) {
-  console.log(data)
-  for (el in data.message) {
-    let dogBreed = document.createElement('li')
-
-    dogBreed.textContent = el
-    dogList.append(dogBreed)
-    dogBreed.addEventListener('click', () => {
-      dogBreed.style.color = "red"
-    })
-  }
-}
+  const liList = document.getElementsByTagName('li')
+  for (let i=0; i < liList.length; i++) {
+    let listItem = liList[i].innerHTML;
+    if (listItem.charAt(0) === dropdownPick) {
+      liList[i].style.display = 'list-item';
+    }
+      else {
+        liList[i].style.display = 'none'
+      }
+    }
+  })
